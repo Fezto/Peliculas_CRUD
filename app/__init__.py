dd434bb13ad366ac59c5ba345bb3647746344927
Ayrton Sepúlveda <ayrtonsepch@gmail.com>
2024-06-10 19:56:57 -0600

    Versión 11.1
    Agregado de .env

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 
+import os
+from dotenv import load_dotenv
+
 #* Generamos la instancia de Flask
 app = Flask(__name__)
 
+#* Traemos las credenciales de nuestro .env
+
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
 #* Configuramos la llave secreta para el funcionamiento de Flask-WTF y Flask-SQLAlchemy
-app.config['SECRET_KEY'] = "unstringdificildeadivinar"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 #* Configuramos la conexión de la app hacia la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Spongebob400!@localhost/chacharitas'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 
 bootstrap = Bootstrap5(app)
 database = SQLAlchemy(app)
